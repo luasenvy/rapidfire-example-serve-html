044ca84c840d6cdab8a3242d80d9a1e40518a0a1
luasenvy <luas.envy@gmail.com>
2021-02-09 01:01:01 +0900

    improve code

--- src/services/HtmlService.js
+++ src/services/HtmlService.js
@@ -1,10 +1,12 @@
 const path = require('path')
 
 const {
-  Interfaces: { Service },
+  Interfaces: { Service, ServiceLoader },
 } = require('@luasenvy/rapidfire')
 
 class HtmlService extends Service {
+  static loader = ServiceLoader
+
   /**
    * [constructor description]
    * @return {[type]} options.router     [description]
